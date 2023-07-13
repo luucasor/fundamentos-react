9f82309967067170a1077d497a4cee88cb6fe5b9
luucasor <lucas@jornadatecnologia.com>
2023-07-13 17:26:13 -0300

    exemplo com o uso de fragment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div id="app">
@@ -12,6 +13,7 @@ ReactDOM.render(
             titulo="Situação do Aluno" 
             aluno="Pedro Silva" 
             nota={9.3}/>
+        <Fragmento />
     </div>,
     document.getElementById("root")
 )
