92645da277db8ae9c34c8d5a8702a2917de7cada
luucasor <lucas@jornadatecnologia.com>
2023-07-18 16:33:35 -0300

    Pegando parâmetros do componente pai via operador spread '...'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import React from 'react'
 
+import Familia from "./components/basicos/Familia"
 import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
@@ -13,6 +14,11 @@ export default () => (
         <h1>Fundamento React (Arrow)</h1>
         
         <div className="Cards">
+            <Card titulo="#05 - Componente com filho" color="#00C8F8">
+                <Familia sobrenome="Ferreira">
+                </Familia>
+            </Card>
+
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60}/>
             </Card>
