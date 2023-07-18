67f981999cfa7c6ed92e789dfff539789159b5bb
luucasor <lucas@jornadatecnologia.com>
2023-07-18 16:17:23 -0300

    Passando estilo por parâmetro para o componente filho

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,21 @@ export default () => (
         <h1>Fundamento React (Arrow)</h1>
         
         <div className="Cards">
-            <Card titulo="#04 - Desafio Aleatório">
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60}/>
             </Card>
 
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
             
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParametro
                     titulo="Situação do Aluno"
                     aluno="Pedro Silva"
                     nota={9.3} />
             </Card>
-            <Card titulo="#01 - Primeiro">
+            <Card titulo="#01 - Primeiro" color="#588C73">
                 <Primeiro />
             </Card>
         </div>
