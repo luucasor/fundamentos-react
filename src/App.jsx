5552dc21bca642b2f066cf527ca32e61026dc59d
luucasor <lucas@jornadatecnologia.com>
2023-07-18 15:59:28 -0300

    Organizando apresentação dos cards de exercicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
+import "./App.css"
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
-import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/layout/Card'
+import Aleatorio from './components/basicos/Aleatorio'
+import Fragmento from './components/basicos/Fragmento'
+import ComParametro from './components/basicos/ComParametro'
+import Primeiro from './components/basicos/Primeiro'
 
+// eslint-disable-next-line
 export default () => (
-    <div id="app">
+    <div className="App">
         <h1>Fundamento React (Arrow)</h1>
         
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={1} max={60}/>
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio min={1} max={60}/>
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
-        
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro Silva"
-                nota={9.3} />
-        </Card>
-        <Card titulo="#01 - Primeiro">
-            <Primeiro />
-        </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3} />
+            </Card>
+            <Card titulo="#01 - Primeiro">
+                <Primeiro />
+            </Card>
+        </div>
     </div>
 )
