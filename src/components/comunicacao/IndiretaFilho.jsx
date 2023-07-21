0cc8b3b445745a0d998b2e1a8878371e26779a24
luucasor <lucas@jornadatecnologia.com>
2023-07-21 16:14:14 -0300

    #12 - Contador - Separação de divs em vários componentes, separado por funcionalidade

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,6 +1,4 @@
 export default function IndiretaFilho(props) {
-    const min = 50
-    const max = 70
     const gerarIdade = () => parseInt(Math.random() * (20)) + 50
     const gerarNerd = () => Math.random() > 0.5
     return (
--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import "./Contador.css"
+import Display from "./Display"
+import Botoes from "./Botoes"
+import PassoForm from "./PassoForm"
 
 export default class Contador extends React.Component {
 
@@ -20,9 +23,9 @@ export default class Contador extends React.Component {
         })
     }    
 
-    setPasso = (evento) => {
+    setPasso = (novoPasso) => {
         this.setState({
-            passo: +evento.target.value
+            passo: novoPasso
         })
     }
 
@@ -30,17 +33,9 @@ export default class Contador extends React.Component {
         return (
             <div className="Contador">
                 <h2>Contador</h2>
-                <h3>{this.state.numero}</h3>
-                <div>
-                    <label htmlFor="passoInput">Passo: </label>
-                    <input 
-                        id="passoInput" 
-                        type="number" 
-                        value={this.state.passo} 
-                        onChange={this.setPasso}/>
-                </div>
-                <button onClick={this.inc}>+</button>
-                <button onClick={this.dec}>-</button>
+                <Display numero={this.state.numero}/>
+                <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+                <Botoes setInc={this.inc} setDec={this.dec}/>
             </div>
         )
     }
