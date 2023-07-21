a815095516861f90399d77d2d7e946d428585b3e
luucasor <lucas@jornadatecnologia.com>
2023-07-21 11:20:32 -0300

    Exercício 10 - Comunicação indireta (filho para pai com uso do hook 'useState')

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,9 +1,14 @@
 export default function IndiretaFilho(props) {
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>Filho</div>
-            <button onClick={() => { props.quandoClicar('João', 53, true)}
+            <button onClick={() => { props.quandoClicar('João', gerarIdade(), gerarNerd())}
             }>Fornecer Informações</button>
+            <div>{Math.random()}</div>
         </div>
     )
 }
